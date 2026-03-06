1e5726d5232c3714ef79087210ffe1d928a5b1e5
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-03-06 10:29:07 +0200

    Added swagger docs logic

--- src/main/java/gr/aueb/cf/eduapp/EduAppApplication.java
+++ src/main/java/gr/aueb/cf/eduapp/EduAppApplication.java
@@ -3,9 +3,11 @@ package gr.aueb.cf.eduapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class EduAppApplication {
 
 	public static void main(String[] args) {
